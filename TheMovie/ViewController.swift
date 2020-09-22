cc0b2b66acb9246d657e30707df3d2755a10bd54
김호종 <herohjk@herohjk.com>
2020-09-22 18:44:46 +0900

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by 김호종 on 2020/09/22.
+//  Copyright © 2020 김호종. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
